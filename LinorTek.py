724aba6a77424a8e84dc825ac2bb572da31d317a
allenvancil <allenvancil@hotmail.com>
2025-03-07 23:11:02 -0800

    combining files, auto ping

--- LinorTek.py
+++ LinorTek.py
@@ -1,9 +1,10 @@
 import re
 import os
 import platform
+from reg_test import Dev_list
+import pandas as pd
 
-
-with open('../data/linortek.txt', 'r') as file:
+with open('PalmerDev_ip.txt', 'r') as file:
     
     l = file.readlines() # give a block list with \n included in each entry
     
@@ -11,32 +12,40 @@ ip_reg = r"\b(?:\d{1,3}\.){3}\d{1,3}\b"  #reg express the get ip address
 
 
 # Making a list of ips of each school
-ip_list = []
-for i in range(len(l)):
-    ip = l[i]
-    ip_addr = re.search(ip_reg, ip)
-    if ip_addr:
-        ip_list.append(ip_addr.group())
-        
-    else:
-        pass
+def Dev_ip():
+    ip_list = []
+    for i in range(len(l)):
+        ip = l[i]
+        ip_addr = re.search(ip_reg, ip)
+        if ip_addr:
+            ip_list.append(ip_addr.group())
+            
+        else:
+            pass
+    ip_list = pd.DataFrame(ip_list)
+    ip_list = ip_list.values
+    return ip_list
 
+LL = Dev_list()
+II = Dev_ip()
+All = LL+', '+II
+print(All[2][0])
 
-def ping_ip(ip_list):
+# def ping_ip(ip_list):
     
-    for i in ip_list:
+#     for i in ip_list:
 
-            if platform.system().lower() == "windows":
-                cmd = f"ping -n 1 {i}"
-            else:
-                cmd = f"ping -c 1 {i}"
+#             if platform.system().lower() == "windows":
+#                 cmd = f"ping -n 1 {i}"
+#             else:
+#                 cmd = f"ping -c 1 {i}"
 
-            response = os.system(cmd)
+#             response = os.system(cmd)
 
-            if response == 0:
-                print(f"Ping to {i} sucessful.")
+#             if response == 0:
+#                 print(f"Ping to {i} sucessful.")
                 
-            else:
-                print(f"Ping to {i} not sucessful")
+#             else:
+#                 print(f"Ping to {i} not sucessful")
 
-ping_ip(ip_list)
+# ping_ip(ip_list)
--- reg_test.py
+++ reg_test.py
@@ -1,27 +1,30 @@
 import re
-import csv
+#import csv
 import pandas as pd
 
-with open("../data/linortek.txt") as file:
+with open("PalmerDev_ip.txt", 'r') as file:
     lines = file.readlines()  # 
 
-reg = r"\w([A-Za-z\s]+)" # this is regualar express for the school
+reg = r"\w([A-Za-z-_0-9\s]+)" # this is regualar express for the school
 line_list = []
-school_list = []
 
-for line in lines:
-    line_list.append(line.strip())
-    school = re.search(reg, line)
-    
-    if school:
-        school_list.append(school.group())
-    else:
-        pass
-school_list = school_list[1::]  # remove the 1st line
 
-school_list = pd.DataFrame(school_list)
-school_list = school_list.values
-print(school_list)
+def Dev_list():
+    school_list = []
+    for line in lines:
+        line_list.append(line.strip())
+        school = re.search(reg, line)
+        
+        if school:
+            school_list.append(school.group())
+        else:
+            pass
+    school_list = school_list[1::]  # remove the 1st line
+
+    school_list = pd.DataFrame(school_list)
+    school_list = school_list.values
+    return school_list
+
 
 
 #school_list.to_csv("../data/School_list.csv")
