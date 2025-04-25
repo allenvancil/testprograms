77a610b5f0faf4de6d4ea877417be0e363e2b55a
Allen Vancil <allenvancil@hotmail.com>
2025-04-24 20:47:02 -0800

    small changes to spdtst

--- speedTest_py.py
+++ speedTest_py.py
@@ -2,6 +2,7 @@ import speedtest
 import matplotlib.pyplot as plt
 import time
 from datetime import datetime
+import pandas as pd
 
 DL = []
 UL = []
@@ -11,8 +12,8 @@ timestamps = []
 def speedTest():
     st  = speedtest.Speedtest()
 
-    print("finding best server...")
-    st.get_best_server()
+    # print("finding best server...")
+    # st.get_best_server()
 
     print('\nDownload speeds...')
     DLspd = st.download()/1_000_000
@@ -22,6 +23,16 @@ def speedTest():
     ULspd = st.upload()/1_000_000
     #print(ULspd/1000000, 'Mbps')
     return DLspd, ULspd
+
+def update_plot():
+    ax.clear()
+    ax.plot(timestamps, DL, label='Downloads (Mbps)', color='blue', marker='o')
+    ax.plot(timestamps, UL, label='Uploads (Mbps)', color='red', marker='x')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Speed (Mbps)')
+    ax.legend()
+    ax.grid(True)
+    plt.pause(1)
 
 plt.ion()  ## this allows live plotting
 
@@ -36,17 +47,17 @@ try:
         UL.append(ULspd)
         timestamps.append(time_now)
 
-        ax.clear()
-        ax.plot(timestamps, DL, label='Downloads (Mbps)', color='blue', marker='o')
-        ax.plot(timestamps, UL, label='Uploads (Mbps)', color='red', marker='x')
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Speed (Mbps)')
-        ax.legend()
-        ax.grid(True)
-        plt.pause(0.1)
-        time.sleep(120)
+
+
+        
+
+        update_plot()
+        time.sleep(60)
 
 except KeyboardInterrupt:
+    df = {'time' : timestamps, 'Download' : DL, 'Uploads' : UL}
+    df = pd.DataFrame(df)
+    df.to_csv('speedtest_doc.csv')
     print("User stopped measurements...")
     plt.ioff()
     plt.show()
