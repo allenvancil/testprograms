e110733edebcb3d2d44073760a1263b89bbe3447
Allen Vancil <allenvancil@hotmail.com>
2025-04-24 23:03:07 -0800

    try again

--- speedTest_py.py
+++ speedTest_py.py
@@ -24,40 +24,20 @@ def speedTest():
     #print(ULspd/1000000, 'Mbps')
     return DLspd, ULspd
 
-def update_plot():
-    ax.clear()
-    ax.plot(timestamps, DL, label='Downloads (Mbps)', color='blue', marker='o')
-    ax.plot(timestamps, UL, label='Uploads (Mbps)', color='red', marker='x')
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Speed (Mbps)')
-    ax.legend()
-    ax.grid(True)
-    plt.pause(1)
-
-plt.ion()  ## this allows live plotting
-
-fig, ax = plt.subplots()
 
 try:
     while True:
         DLspd, ULspd = speedTest()
+        print('\nappending data...\n')
         time_now = datetime.now().strftime('%H:%M:%S')
 
         DL.append(DLspd)
         UL.append(ULspd)
         timestamps.append(time_now)
-
-
-
-        
-
-        update_plot()
-        time.sleep(60)
+        time.sleep(120)
 
 except KeyboardInterrupt:
     df = {'time' : timestamps, 'Download' : DL, 'Uploads' : UL}
     df = pd.DataFrame(df)
     df.to_csv('speedtest_doc.csv')
     print("User stopped measurements...")
-    plt.ioff()
-    plt.show()
